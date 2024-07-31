3638a43211f5d95901be11f5e552804b41ba2b56
Eros Eloi <eroseloi92@gmail.com>
2024-07-31 15:51:40 -0300

    closes #4 - implementar as 4 operacoes matematicas

--- script.js
+++ script.js
@@ -2,11 +2,29 @@
 
 
 
-function calc(){
+function calc(operacoes){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
 
-    var result = (num1 + num2);
+    var result;
+
+
+    switch(operacoes){
+        case '+': result = num1 + num2;
+        break;
+
+        case '-': result = num1 - num2;
+        break;
+
+        case '*': result = num1 * num2;
+        break;
+
+        case '/': result = num1 / num2;
+        break;
+
+        default:
+            result = "invalido";
+    }
 
     document.getElementById("result").value = result;
 
