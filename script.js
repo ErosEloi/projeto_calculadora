aba4ba09507f1b27e65460edd5709addb542e966
Eros Eloi <eroseloi92@gmail.com>
2024-07-31 16:29:19 -0300

    closes #5 - logica inicial do historico

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 
 
 
+// funcionaliade da calculadora
+function calc(operacoes) {
 
-function calc(operacoes){
+    // variariaveis
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
 
     var result;
 
-
-    switch(operacoes){
+    // logica das operacoes
+    switch (operacoes) {
         case '+': result = num1 + num2;
-        break;
+            break;
 
         case '-': result = num1 - num2;
-        break;
+            break;
 
         case '*': result = num1 * num2;
-        break;
+            break;
 
         case '/': result = num1 / num2;
-        break;
+            break;
 
         default:
             result = "invalido";
     }
 
+    // resultado
     document.getElementById("result").value = result;
 
+    var newHistory = "<div>" + num1 + " " + operacoes + " " + num2 + " " + " = " + result + "</div>";
+
+    document.getElementById("history").innerHTML = newHistory + document.getElementById("history").innerHTML;
+
 }
+
